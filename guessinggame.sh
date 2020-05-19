646a2083c978d654733eb802842eb53121e54974
GOmar7 <omargarcia.leon@gmail.com>
2020-05-18 23:35:49 -0400

    first commit

--- guessinggame.sh
+++ guessinggame.sh
@@ -5,17 +5,17 @@ source validate.sh
 
 var=$(ls | wc -l)
 
-echo "Indique cuantos archivos hay"
+echo "Indique cuantos archivos hay | indicate how many files are there"
 read "texto"
 
 while [[ "$texto" != "$var" ]]
 	do
-		echo "Error intente nuevamente"$validate
+		echo "Error intente nuevamente | Error, please try again"$validate
 		read "texto"
 	done
 if [ "$texto" = "$var" ]; then
 echo "Respuesta correcta, hay un total de "$var "ficheros"
-echo "Congratulations"
+echo "Correct answer, congratulations! there are "$var "files in this directory".
 else
-echo "Incorrecto, intente nuevamente"
+echo "Incorrecto, intente nuevamente | Incorrect, try again please"
 fi
--- readme.md
+++ readme.md
@@ -1,1 +1,3 @@
-Hi this is my work
+#Hi this is my work
+Please execute this file [guessinggame.sh](https://github.com/GOmar7/my-final-work-og/blob/master/guessinggame.sh)
+#Thanks for the feedback
